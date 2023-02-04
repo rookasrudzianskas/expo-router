c04db93b782b8d09394176370803fcb54a416c38
Rokas <rookas.rudzenskas@gmail.com>
2023-02-04 18:46:09 +0200

    Add icons to the bottom tab bar
    - Add icons to the bottom tab bar
    - Add icons to the bottom tab bar
    - Add icons to the bottom tab bar

--- app/home/_layout.js
+++ app/home/_layout.js
@@ -1,9 +1,29 @@
 import { Tabs } from 'expo-router';
-
+import {FontAwesome } from '@expo/vector-icons';
 export default () => {
   return <Tabs screenOptions={{
     tabBarShowLabel: false,
   }} >
-    <Tabs.Screen name="feed" />
+    <Tabs.Screen name="feed" options={{
+      tabBarIcon: ({ focused }) => {
+        return (
+          <FontAwesome name="home" size={24} color={focused ? '#1e2632' : '#c41212'} />
+        )
+      }
+    }} />
+    <Tabs.Screen name="search" options={{
+      tabBarIcon: ({ focused }) => {
+        return (
+          <FontAwesome name="search" size={24} color={focused ? '#1e2632' : '#c41212'} />
+        )
+      }
+    }} />
+    <Tabs.Screen name="messages" options={{
+      tabBarIcon: ({ focused }) => {
+        return (
+          <FontAwesome name="user" size={24} color={focused ? '#1e2632' : '#c41212'} />
+        )
+      }
+    }} />
   </Tabs>
 }
