d3215bd660d1d963fbc41dd35096439b36d2a8c8
Rokas <rookas.rudzenskas@gmail.com>
2023-02-04 17:10:33 +0200

    feat: add profile page
    - Add a profile page
    - Add a link to the profile page from the home page
    - Add a button to go back to the home page from the profile page

--- app/profile.js
+++ app/profile.js
@@ -1,15 +1,30 @@
 //@ts-nocheck
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, Button} from 'react-native';
+import { useRouter, useSearchParams } from 'expo-router';
 
 const Profile = () => {
+  const router = useRouter();
+  const params = useSearchParams();
+  console.log(params.name);
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyItems: 'center'}}>
+    <View style={styles.container}>
       <Text>
         byrookas 🚀
       </Text>
+      <Button onPress={() => router.back()} title="Go Back" />
     </View>
   );
 };
 
 export default Profile;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 24,
+    justifyContent: "center",
+  },
+
+});
